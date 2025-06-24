276f63f8c21c347b862f19e4829d0ed647893e11
andre <kinosite@gmail.com>
2025-06-24 06:41:20 +0300

    Add task_02 package and classes

---
+++ src/task_02/FirstClass.java
@@ -0,0 +1,9 @@
+package task_02;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        /*
+
+         */
+    }
+}
---
+++ src/task_02/ThirdClass.java
@@ -0,0 +1,9 @@
+package task_02;
+
+public class ThirdClass {
+    public static void main(String[] args) {
+        /*
+
+         */
+    }
+}
